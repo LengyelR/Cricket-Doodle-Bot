5fdb699a8abbafe8b05285ee67597e7154622217
LengyelR <richard.lengyel@yahoo.com>
2017-09-11 23:32:37 +0100

    improvements
    creating screenshot, slicing it afterwards
    better normalisation of score picture (preprocess)

--- cricket/bot.py
+++ cricket/bot.py
@@ -6,24 +6,58 @@ import os
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import cricket.utils as utils
-import time
 from PIL import Image
+
+
+def preprocess(mtx):
+    im = Image.fromarray(mtx)
+    im = im.convert('L')
+
+    area = (20, 5, 34, 18)
+    cropped_img = im.crop(area)
+
+    mtx = np.array(cropped_img)
+    mtx[mtx < 144] = 0
+    mtx[:, 0] = 0
+
+    for index, col in enumerate(mtx.T):
+        if np.count_nonzero(col) in (1, 2):
+            mtx[:, index] = np.zeros(13)
+
+    non_empty_columns = np.where(mtx.max(axis=0) > 0)[0]
+    non_empty_rows = np.where(mtx.max(axis=1) > 0)[0]
+
+    border = (min(non_empty_rows), max(non_empty_rows)+1,
+              min(non_empty_columns), max(non_empty_columns)+1)
+
+    cut = mtx[border[0]:border[1], border[2]:border[3]]
+    row = cut.shape[0]
+    col = cut.shape[1]
+
+    row_start = (14 - row) // 2
+    row_end = row_start + row
+    col_start = (14 - col) // 2
+    col_end = col_start + col
+
+    paste = np.zeros((14, 14))
+    paste[row_start:row_end, col_start:col_end] = cut
+
+    return paste
 
 
 class CricketBot:
     region_loc = os.path.join('res', 'region.PNG')
 
-    def __init__(self, region, score, image_path, nwc):
+    def __init__(self, zero, image_path, nwc):
         self.red = (34, 34, 187, 255)
-        self.region = region
-        self.score_region = score
-        self.click = (region[0], region[1])
+        self.zero = zero
+        self.click = (zero[0]+278, zero[1]+274)
         self.image_path = image_path
         self.network_checkpoint = nwc
 
-    def grab_screen(self, area=None):
-        left, top, x2, y2 = self.score_region if area is None else area
+    @staticmethod
+    def grab_screen(area):
+        left, top, x2, y2 = area
         width = x2 - left + 1
         height = y2 - top + 1
 
@@ -45,42 +79,31 @@ class CricketBot:
         win32gui.ReleaseDC(win32gui.GetDesktopWindow(), hwindc)
         return img
 
-    def read_score(self):
-        score_pic = bot.grab_screen()
-        image = Image.fromarray(score_pic)
-        image = image.resize((28, 28))
-
-        mtx = np.array(image)
-        bw = mtx[:, :, 2]
+    @staticmethod
+    def read_score(score_mtx):
+        image = Image.fromarray(score_mtx)
+        bw = np.array(image.convert('L'))
         bw[bw < 150] = 0
-        bw[:, 0:4] = 0
-        bw[:, -3:] = 0
-        bw[0, :] = 0
-        bw[-1:, :] = 0
 
         non_empty_columns = np.where(bw.max(axis=0) > 0)[0]
         non_empty_rows = np.where(bw.max(axis=1) > 0)[0]
-
-        if len(non_empty_columns) == 0 \
-                or len(non_empty_rows) == 0\
-                or np.sum(bw[8:15, 8:15]) > 200*49 \
-                or np.sum(bw) < 1000:
-            time.sleep(0.05)
-            return None
 
         border = (min(non_empty_rows), max(non_empty_rows), min(non_empty_columns), max(non_empty_columns))
         cut = bw[border[0]:border[1], border[2]:border[3]]
         row = cut.shape[0]
         col = cut.shape[1]
 
-        row_start = (28 - row) // 2
+        row_start = (14 - row) // 2
         row_end = row_start + row
-        col_start = (28 - col) // 2
+        col_start = (14 - col) // 2
         col_end = col_start + col
 
-        paste = np.zeros((28, 28))
+        paste = np.zeros((14, 14))
         paste[row_start:row_end, col_start:col_end] = cut
-        return paste.reshape(1, 784)
+        image = Image.fromarray(paste)
+        image = image.resize((28, 28))
+        mtx = np.array(image)
+        return mtx.reshape(1, 784)
 
     def ball_detected(self, mtx):
         for x in mtx:
@@ -92,73 +115,46 @@ class CricketBot:
         print("bot... running...")
         i = 0
         counter = 0
-        click_counter = 0
-        ball_was_hit = False
-        possible_end_of_ep = utils.make_square((526, 313), 10)
-
         loaded_graph = tf.Graph()
 
         with tf.Session(graph=loaded_graph) as sess:
-            saver = tf.train.import_meta_graph('../digit/' + checkpoint + '/digit.ckpt.meta')
-            saver.restore(sess, tf.train.latest_checkpoint('../digit/' + checkpoint))
+            saver = tf.train.import_meta_graph(self.network_checkpoint + '/digit.ckpt.meta')
+            saver.restore(sess, tf.train.latest_checkpoint(self.network_checkpoint))
 
             y_conv = loaded_graph.get_tensor_by_name("fc2/y_conv_model:0")
             x_tensor = loaded_graph.get_tensor_by_name("input_x:0")
             keep_prob_tensor = loaded_graph.get_tensor_by_name("dropout/keep_prob:0")
+
             while True:
                 i += 1
                 if i % 250 == 0:
                     print('frames:', i)
 
-                mtx = self.grab_screen(self.region)
-                new_throw = self.grab_screen(possible_end_of_ep)
+                mtx = self.grab_screen((self.zero[0], self.zero[1],
+                                        self.zero[0]+538, self.zero[1]+300))
+                score_mtx = mtx[22:45, 253:285]
+                incoming_ball_mtx = mtx[187:210, 254:279]
 
-                if self.ball_detected(mtx):
+                if self.ball_detected(incoming_ball_mtx):
                     pyautogui.click(self.click)
-                    click_counter += 1
-                    print('CLICK:', click_counter)
-                    ball_was_hit = True
 
-                if ball_was_hit and self.ball_detected(new_throw):
+                    print('CLICK:', counter)
                     counter += 1
-                    score = self.read_score()
-                    if score is None:
-                        print('UNKNOWN:', counter)
-                        ball_was_hit = False
-                        continue
+                    digit_mtx = preprocess(score_mtx)
 
-                    res = sess.run(y_conv, feed_dict={x_tensor: score, keep_prob_tensor: 1.0})
-                    prediction = np.argmax(res)
+                    res = sess.run(y_conv, feed_dict={x_tensor: digit_mtx.reshape(1, 196), keep_prob_tensor: 1.0})
+                    digit = np.argmax(res)
 
-                    pairs = []
-                    for i, s in enumerate(res[0]):
-                        pairs.append((i, s))
-                    top = sorted(pairs, key=lambda k: k[1], reverse=True)[:3]
-
-                    s = ''
-                    total = sum([k[1] for k in top])
-                    for pair in top:
-                        s += f'({pair[0]}, {round(pair[1]/total, 2)}) '
-                    print(s)
-                    conf = round(top[0][1]/total, 2)
-
-                    utils.write_array(score[0], str(prediction), self.image_path + 'logs.txt')
-                    utils.write_array(s, str(counter), self.image_path + 'cc.txt')
-                    plt.imsave(arr=score.reshape((28, 28)),
-                               fname=f"{self.image_path}{counter}_{conf}_{prediction}.png",
+                    plt.imsave(arr=digit_mtx,
+                               fname=f"{self.image_path}{digit}_{counter}.png",
                                cmap='gray')
-                    print('EPISODE:', counter)
-                    ball_was_hit = False
-
 
 if __name__ == "__main__":
-    checkpoint = 'cricket_checkpoints_drop'
-    log = 'C:\\tmp\images\\game8\\'
+    checkpoint = '../digit/convnet/convnet_checkpoint'
+    log = 'C:\\tmp\images\\game9\\'
+    f = lambda x: Image.fromarray(x).show()
 
-    top_left_game = (497, 382)
-    top_left_score = (526, 235)
-    game = utils.make_square(top_left_game, 50)
-    score_region = utils.make_square(top_left_score, 12)
+    top_left_game = (253, 208)
 
-    bot = CricketBot(game, score_region, log, checkpoint)
+    bot = CricketBot(top_left_game, log, checkpoint)
     bot.start()
